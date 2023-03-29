92de264a9db1d83253ff9b8e8c001a4878cb7205
jarigantvoort <j.gantvoort@xsarus.nl>
2023-03-29 11:19:39 +0200

    test

--- oefening/app.js
+++ oefening/app.js
@@ -3,12 +3,40 @@ import { createChart, updateChart } from "./scatterplot.js"
 //
 // demo data
 //
-const data = [
-        { horsepower: 130, mpg: 18 },
-        { horsepower: 165, mpg: 15 },
-        { horsepower: 225, mpg: 14 },
-        { horsepower: 97, mpg: 18 },
-        { horsepower: 88, mpg: 27 },
-        { horsepower: 193, mpg: 9 },
-        { horsepower: 80, mpg: 25 },
-]
+
+function loadData(){
+        Papa.parse("./data/cars.csv", {
+            download:true,
+            header:true, 
+            dynamicTyping:true,
+            complete: results => checkData(results.data)
+        })
+    }
+
+function checkData(data) {
+        const chartdata = data.map(car => ({
+                x: car.horsepower,
+                y: car.mpg,
+            }))
+        console.log(chartdata)
+            createChart(chartdata, "Horsepower", "MPG")
+}
+
+// const data = [
+//         { horsepower: 130, mpg: 18 },
+//         { horsepower: 165, mpg: 15 },
+//         { horsepower: 225, mpg: 14 },
+//         { horsepower: 97, mpg: 18 },
+//         { horsepower: 88, mpg: 27 },
+//         { horsepower: 193, mpg: 9 },
+//         { horsepower: 80, mpg: 25 },
+// ]
+
+
+
+// kijk hoe de data eruit ziet
+// console.log(chartdata)
+
+// chartjs aanmaken
+
+loadData()
