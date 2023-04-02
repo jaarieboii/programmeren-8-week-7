dfc13e8fc0fdb1530512db8455d51df2b66f417f
jarigantvoort <j.gantvoort@xsarus.nl>
2023-04-02 22:33:52 +0200

    test model

--- inleveropdracht/train.js
+++ inleveropdracht/train.js
@@ -2,8 +2,10 @@ import { createChart, updateChart } from "./scatterplot.js"
 
 const nn = ml5.neuralNetwork({ task: 'regression', debug: true })
 
+const field = document.getElementById("field")
+
 function loadData(){
-        Papa.parse("./data/mobilephones.csv", {
+        Papa.parse("./data/cars.csv", {
             download:true,
             header:true, 
             dynamicTyping:true,
@@ -12,62 +14,57 @@ function loadData(){
     }
 
 function checkData(data) {
-    console.table(data)
-        // const chartdata = data.map(car => ({
-        //         x: car.horsepower,
-        //         y: car.mpg,
-        //     }))
-        // console.log(chartdata)
-        // createChart(chartdata, "Horsepower", "MPG")
+        console.table(data)
+        data.sort(() => (Math.random() - 0.5))
 
-        // // shuffle
-        // data.sort(() => (Math.random() - 0.5))
+        let trainData = data.slice(0, Math.floor(data.length * 0.8))
+        let testData = data.slice(Math.floor(data.length * 0.8) + 1)
 
-        // // een voor een de data toevoegen aan het neural network
-        // for (let car of data) {
-        // nn.addData({ horsepower: car.horsepower }, { mpg: car.mpg })
-        // }
-
-        // // normalize
-        // nn.normalizeData()
-        // startTraining()
-}
-
-function startTraining() {
-        nn.train({ epochs: 10 }, () => finishedTraining()) 
+        for(let car of trainData){
+                nn.addData({ 
+                        horsepower: car.horsepower
+                }, {mpg:car.mpg})
+        }
+        //normalize Data and start training
+        nn.normalizeData()
+        nn.train({ epochs: 10 }, () => finishedTraining())
+        
+        //draw scatterplot
+        const chartdata = data.map(car => ({
+                x: car.horsepower,
+                y: car.mpg,
+            }))
+        console.log(chartdata)
+        createChart(chartdata, "Horsepower", "MPG")
 }
 
 
 async function finishedTraining() {
-        let predictions = []
+        let predictions = [];
+
         for (let hp = 40; hp < 250; hp += 2) {
-                const pred = await nn.predict({horsepower: hp})
-                predictions.push({x: hp, y: pred[0].mpg})
-        }
+                const pred = await nn.predict({ horsepower: hp });
+                predictions.push({ x: hp, y: pred[0].mpg });
+              }
+
+        console.log("updated")
         updateChart("Predictions", predictions)
 }
 
 async function makePrediction() {
-        const results = await nn.predict({ horsepower: 60 })
+        let valueInt = parseInt(field.value)
+        const results = await nn.predict({ horsepower: valueInt })
         console.log(`Geschat verbruik: ${results[0].mpg}`)
+        document.getElementById('result').innerHTML = `De geschatten miles per gallon afgerond op twee decimalen: ${parseFloat(results[0].mpg).toFixed(2)}`
 }
 
+function saveModel() {
+        nn.save()
+}
 
-// const data = [
-//         { horsepower: 130, mpg: 18 },
-//         { horsepower: 165, mpg: 15 },
-//         { horsepower: 225, mpg: 14 },
-//         { horsepower: 97, mpg: 18 },
-//         { horsepower: 88, mpg: 27 },
-//         { horsepower: 193, mpg: 9 },
-//         { horsepower: 80, mpg: 25 },
-// ]
+document.getElementById('btn').addEventListener('click', makePrediction)
 
-
-
-// kijk hoe de data eruit ziet
-// console.log(chartdata)
-
-// chartjs aanmaken
+document.getElementById('save').addEventListener('click', saveModel)
 
 loadData()
+
--- oefening/train.js
+++ oefening/train.js
@@ -56,21 +56,5 @@ async function makePrediction() {
 }
 
 
-// const data = [
-//         { horsepower: 130, mpg: 18 },
-//         { horsepower: 165, mpg: 15 },
-//         { horsepower: 225, mpg: 14 },
-//         { horsepower: 97, mpg: 18 },
-//         { horsepower: 88, mpg: 27 },
-//         { horsepower: 193, mpg: 9 },
-//         { horsepower: 80, mpg: 25 },
-// ]
-
-
-
-// kijk hoe de data eruit ziet
-// console.log(chartdata)
-
-// chartjs aanmaken
 
 loadData()
